2c321ef11878aeac4eb5a979fd7552c0c81d6afa
DenysLobur <denismachtalles@gmail.com>
2024-01-12 19:00:28 +0200

    Making the fleet move

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -33,6 +33,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -80,6 +81,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update position"""
+        self._check_flet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         """Creates the fleet of aliens"""
         # Create an alien and keep adding aliens until there's no room left
@@ -104,6 +110,19 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_flet_edges(self):
+        """Respond appropriately if any alien reached the edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet direction"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screem and flip to next screen"""
