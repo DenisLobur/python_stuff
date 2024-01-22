ec6b51627b728b50f562fc6be6b4a3477b3355d4
DenysLobur <denismachtalles@gmail.com>
2024-01-22 19:35:43 +0200

    Leveling up ship speed, bullet speed and alien speed

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,16 +9,35 @@ class Settings:
         self.bg_color = (230, 230, 230)
         self.game_name = "Alien Invasion"
         # Bullet settings
-        self.bullet_speed = 20.0
         self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+        # Ship settings
+        self.ship_limit = 3
+
+        # How quickly the game speeds up
+        self.speed_upscale = 1.1
+
+        self.initialize_dynamic_settings()
+
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+
         # fleet direction of 1 represents right, -1 represents left
         self.fleet_direction = 1
-        # Ship settings
-        self.ship_speed = 1.5
-        self.ship_limit = 3
+
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speed_upscale
+        self.bullet_speed *= self.speed_upscale
+        self.alien_speed *= self.speed_upscale
+
+
